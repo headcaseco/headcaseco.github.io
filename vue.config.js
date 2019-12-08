e8323ba43607b7c475b836ba24a31c6423d8b93d
derek <derek.chiampas@gmail.com>
2019-12-08 00:03:43 -0800

    replaces vue project with dist dir

--- vue.config.js
+++
@@ -1,4 +0,0 @@
-//NOTE: deploy instructions: https://medium.com/@Roli_Dori/deploy-vue-cli-3-project-to-github-pages-ebeda0705fbd
-module.exports = {
-  publicPath: "https://headcaseco.github.io/"
-}
